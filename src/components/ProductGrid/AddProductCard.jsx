92d4e35c36353490ce36e488f57c23bef522f7c3
shaurya1994 <shaurya_agent47@yahoo.in>
2025-10-15 11:18:22 +0530

    Removing commented code

--- src/components/ProductGrid/AddProductCard.jsx
+++ src/components/ProductGrid/AddProductCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { gridStyles } from "./ProductGrid.styles";
 
 const AddProductCard = ({ onClick }) => {
@@ -11,18 +10,3 @@ const AddProductCard = ({ onClick }) => {
 };
 
 export default AddProductCard;
-
-// import { productGridStyles as s } from "./ProductGrid.styles";
-
-// const AddProductCard = ({ onClick }) => (
-//   <div
-//     style={s.addCard}
-//     className="add-card"
-//     onClick={onClick}
-//   >
-//     <div style={s.addBox}>+</div>
-//     <h6 style={s.addText}>Add Product</h6>
-//   </div>
-// );
-
-// export default AddProductCard;
